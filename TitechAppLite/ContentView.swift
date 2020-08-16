49e9f7a35c60aa3003850d49bf6ad6894c220d8a
マイケル吾郎 <tacicamori@gmail.com>
2020-08-16 14:19:04 +0900

    Initial Commit

---
+++ TitechAppLite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TitechAppLite
+//
+//  Created by 野田裕介 on 2020/08/16.
+//  Copyright © 2020 michaellgoro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
