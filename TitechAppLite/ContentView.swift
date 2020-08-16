916548af4453785d2760f7d5fb6e5887e51ab5e8
マイケル吾郎 <tacicamori@gmail.com>
2020-08-16 15:22:36 +0900

    最初のコミット

--- TitechAppLite/ContentView.swift
+++ TitechAppLite/ContentView.swift
@@ -9,9 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+   
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+        List(0..<10){_ in
+            HStack{
+                VStack(alignment: .leading){
+                    HStack {
+                        Text("9:00")
+                            .font(.subheadline)
+                        Text("電磁気学")
+                            
+                        Text("【講義室変更】")
+                            .foregroundColor(Color.red)
+                    }
+                    Spacer()
+                    HStack{
+                        Text("10:30")
+                    
+                        Text("Week1 Description")
+                        
+                    }
+                    .foregroundColor(Color.gray)
+                    .font(.subheadline)
+                    
+                }
+                Spacer()
+                Text("W222")
+                    .font(.subheadline)
+                    .foregroundColor(Color(red: 0, green: 1.0, blue: 0.5, opacity: 2.0))
+                    
+            }
+        .padding()
+            
+            }
+        .navigationBarTitle(Text("スケジュール"))
+            
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
